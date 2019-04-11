15383e357f8676a2e034c51694458b6249ee7f5e
Cryptocracy <admin@cryptocracy.io>
2019-04-11 15:14:26 -0700

    updated cryptocracy.js as needed
    updated as neede

--- src/services/cryptocracy.js
+++ src/services/cryptocracy.js
@@ -9,15 +9,15 @@ const cryptocracyServices = {
     return res.data
   },
   countVotes: async (query) => {
-    let res = await Axios.get('https://search.cryptocracy.io/souq/quorum/votes', { params: {...query} })
+    let res = await Axios.get('https://search.cryptocracy.io/remark/popular/votes', { params: {...query} })
     return res.data
   },
   notifyServer: async (fqn) => {
-    let res = await Axios.get(`https://search.cryptocracy.io/souq/quorum/check?user=${fqn}`)
+    let res = await Axios.get(`https://search.cryptocracy.io/remark/popular/check?user=${fqn}`)
     return res.data
   },
   getQuorumContent: async (query) => {
-    let res = await Axios.get('https://search.cryptocracy.io/souq/quorum', { params: {...query} })
+    let res = await Axios.get('https://search.cryptocracy.io/remark/popular', { params: {...query} })
     return res.data
   }
 }
