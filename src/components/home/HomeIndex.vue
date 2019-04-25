dbc35659de1e6c67b05ab169367e9c82f886efbd
Cryptocracy <admin@cryptocracy.io>
2019-04-25 16:58:47 -0700

    Update HomeIndex.vue
    stripped down in preps for aggregate functionality

--- src/components/home/HomeIndex.vue
+++ src/components/home/HomeIndex.vue
@@ -1,141 +1,53 @@
 <template lang="html">
   <div class="container-fluid">
     <div>
-      <!--      <v-layout transition="slide-y-transition" row class="mt-0 mb-3" :class="proximitySearchResult.length > 0 ? 'map-wrapper' : 'map-wrapper-ini'" v-if="proximitySearchResult">
-        <v-flex col >
-          <v-subheader class="pl-0 mt-0"></v-subheader>
-          <OpenMapWithMultipleMarkers
-            v-if="proximitySearchResult"
-            :markers="proximitySearchResult"
-            :center="mapCenter"
-            @updateMap="getNewData"
-            class="dashboardMap"
-          >
-          </OpenMapWithMultipleMarkers>
-        </v-flex>
-      </v-layout>-->
-      <!--      <app-box></app-box>-->
-
       <v-layout row flex-wrap>
-        <v-flex md6>
-          <app-layout
-            height="300px"
-            title="Wallet Input (Received)"
-            :children="LineChart"
-            class='walletInput'
-            :showGraph="showDonationGraph"
-            error_message_title = "No Donations found."
-            error_message="Be patient, it may take time for other users to show interest in your content."
-          >
-          </app-layout>
+        <v-flex md12>
+          <v-card>
+            <h1>Latest Sound Discovery Coming Soon</h1>
+          </v-card>
         </v-flex>
-        <v-flex md6>
-          <app-layout
-            height="300px"
-            title="Wallet Output (Payouts)"
-            :children="BarChart"
-            class='walletOutput'
-            :showGraph="showPayoutGraph"
-            error_message_title = "No Payouts found."
-            error_message="Looks like you have not initiated any payouts yet."
-          >
-          </app-layout>
+        <v-flex md12>
+          <v-card :hover="true" class="br20">
+            <v-card-text>
+              <v-layout row>
+                <v-flex v-if="lazyLoadedData[item.contentUrl]" class="space-between">
+                  <h4 class="inline-block">{{lazyLoadedData[item.contentUrl].title}} TITLE</h4>
+                </v-flex>
+              </v-layout>
+              <div>{{type(item.contentUrl)}}</div>
+              <div class="v-list__tile__sub-title" v-if="lazyLoadedData[item.contentUrl]" >{{lazyLoadedData[item.contentUrl].description}} DETAIL</div>
+              <div class="v-list__tile__sub-title">Storage URL: <span>{{item.contentUrl}}</span></div>
+            </v-card-text>
+            <v-tooltip bottom>
+              <v-btn :disabled="!lazyLoadedData[item.contentUrl]" v-if="!isAdded" @click="$store.commit('MUTATION_ADD_TO_PLAYLIST', lazyLoadedData[item.contentUrl]); isAdded = true" color="primary" slot="activator" round icon ><v-icon>playlist_add</v-icon></v-btn>
+              <v-btn v-else  color="primary" slot="activator" round icon ><v-icon>done</v-icon></v-btn>
+              <span v-if="!isAdded">Add to current playlist</span>
+              <span v-else>Added to current playlist</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+              <v-btn slot="activator" icon color="primary" @click="$store.commit('MUTATION_SET_SOUND', lazyLoadedData[item.contentUrl])" round ><v-icon>play_arrow</v-icon></v-btn>
+              <span>Play</span>
+            </v-tooltip>
+            <div class="v-list__tile__sub-title"><v-btn><v-icon color="red accent-4">favorite_border</v-icon> {{item.votes}} </v-btn> - Created: {{new Date(Number(item.createdOn)).toDateString()}}</div>
+          </v-card>
         </v-flex>
       </v-layout>
     </div>
-
   </div>
 </template>
 
 <script>
-import BoxWrapper from '@/components/icon-box/BoxWrapper'
-import LineChart from '@/components/charts/line-chart/LineChart'
-import BarChart from '@/components/charts/bar-chart/BarChart'
-import storageService from '@/services/blockstack-storage'
-import OpenMapWithMultipleMarkers from '@/components/maps/OpenMapWithMultipleMarkers'
-import { mapGetters } from 'vuex'
 
 export default {
   components: {
-    'app-box': BoxWrapper,
-    OpenMapWithMultipleMarkers
   },
   computed: {
-    ...mapGetters({
-      isSearching: 'isSearching',
-      isResolved: 'isResolved',
-      searchResult: 'getSearchResult',
-      showDonationGraph: 'showDonationGraph',
-      showPayoutGraph: 'showPayoutGraph',
-      settings: 'getSettings',
-      proximitySearchResult: 'getProximitySearchResult'
-    })
   },
   data: () => ({
-    LineChart,
-    BarChart,
-    timer: null,
-    mapCenter: {
-      lat: '',
-      lng: '',
-      radius: ''
-    }
+    timer: null
   }),
-  created () {
-    this.$store.dispatch('ACTION_GET_TRANSACTIONS_DATA')
-    this.$store.dispatch('ACTION_GET_QUORUM_DATA', { sort: 'pop' })
-    storageService.getFile({
-      fileName: 'settings.json',
-      options: {decrypt: true}
-    }).then(res => {
-      if (res) {
-        this.$store.commit('MUTATION_CHANGE_SETTINGS', res)
-        this.searchProximity({ settings: res })
-      } else {
-        this.searchProximity({ settings: this.settings })
-        this.createFile()
-      }
-      this.mapCenter = {
-        lat: res ? res.latitude : this.settings.latitude,
-        lng: res ? res.longitude : this.settings.longitude
-      }
-    })
-  },
-  methods: {
-    createFile () {
-      storageService.putFile({
-        fileName: 'settings.json',
-        data: this.settings,
-        options: {encrypt: true}
-      })
-    },
-    searchProximity (paramObj) {
-      let {settings, query} = paramObj
-      let proximityQuery = query || {
-        lat: settings.latitude,
-        lng: settings.longitude,
-        distance: settings.searchRadius,
-        unit: settings.distanceUnit
-      }
-      this.$store.dispatch('ACTION_PROXIMITY_SEARCH', proximityQuery)
-    },
-    getNewData (center) {
-      let query = {
-        lat: center.lat,
-        lng: center.lng,
-        distance: this.settings.searchRadius,
-        unit: this.settings.distanceUnit
-      }
-      this.debounce(this.searchProximity, 1000)({ query })
-      // this.timer = setTimeout(this.searchProximity({ query }), 500)
-    },
-    debounce (func, delay) {
-      return (args) => {
-        clearTimeout(this.timer)
-        this.timer = setTimeout(() => func.call(this, args), delay)
-      }
-    }
-  }
+  methods: {}
 }
 </script>
 
